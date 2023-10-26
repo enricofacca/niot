8a3af6fb3973b769fe890a6094776f1f1d455d33
enrico facca <enrico.facca@gmail.com>
2023-10-26 10:45:28 +0200

    pytest for image2dat module

--- src/niot/image2dat.py
+++ src/niot/image2dat.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 from PIL import Image
 import numpy as np
-import sys
-import os
-import subprocess
 from copy import deepcopy as cp
 import firedrake as fd
 import matplotlib.pyplot as plt
@@ -18,10 +15,12 @@ def build_mesh_from_numpy(np_image, mesh_type='simplicial'):
             quadrilateral = False
       elif (mesh_type == 'cartesian'):
             quadrilateral = True
-      print(f'Mesh type {mesh_type} quadrilateral {quadrilateral}')
       
+      # here we swap the axes because the image is 
+      # read from left, right, top to bottom
       height, width  = np_image.shape
-      print(f'{width=} {height=}')
+
+      # create mesh
       mesh = fd.RectangleMesh(
             width,
             height,
@@ -39,7 +38,6 @@ def build_mesh_from_numpy(np_image, mesh_type='simplicial'):
             hexahedral = False
       elif (mesh_type == 'cartesian'):
             hexahedral = True
-      print(f'{mesh_type=} {hexahedral=}')
       mesh = fd.BoxMesh(nx=height,
                   ny=width, 
                   nz=depth,  
@@ -52,8 +50,60 @@ def build_mesh_from_numpy(np_image, mesh_type='simplicial'):
                   )
    else:
       raise ValueError('Only 2D and 3D images are supported')
+
+   # the following is needed because (from Firedrake documentation)
+   """
+   Finish the initialisation of the mesh.  Most of the time
+   this is carried out automatically, however, in some cases (for
+   example accessing a property of the mesh directly after
+   constructing it) you need to call this manually.
+   """
+   mesh.init()
+
    return mesh
 
+def get_box_division(mesh):
+   """ 
+   Given a mesh, return the number of divisions in each direction.
+   These informations are lost when the mesh is created.
+   Args:
+      mesh: firedrake mesh
+   Returns:
+      list with the number of divisions in the direction x, y, [z]
+   """
+   # get x and y dimensions   
+   if mesh.geometric_dimension() == 2:
+      # get x and y dimensions
+      nx = mesh.exterior_facets.subset(3).size
+      ny = mesh.exterior_facets.subset(1).size
+      return [nx, ny]
+   elif mesh.geometric_dimension() == 3:
+      # get x, y and z dimensions
+      xy = mesh.exterior_facets.subset(5).size
+      yz = mesh.exterior_facets.subset(1).size
+      xz = mesh.exterior_facets.subset(3).size
+      nx = np.rint(np.sqrt(xy*xz/yz))
+      ny = np.rint(xy/nx)
+      nz = np.rint(xz/nx)
+      return [nx, ny, nz]
+   
+def compatible(mesh, value):
+   """
+   Ceck that mesh and image have the same shape
+   """
+   np_shape = value.shape
+   mesh_shape = get_box_division(mesh)
+   check = True
+   if (len(np_shape) == 2):
+      if (mesh_shape[0] != np_shape[1]) or (mesh_shape[1] != np_shape[0]):
+         check = False
+   elif (len(np_shape) == 3):
+      if (mesh_shape[0] != np_shape[1]) or (mesh_shape[1] != np_shape[0]) or (mesh_shape[2] != np_shape[2]):
+         check = False
+   else:
+      raise ValueError('Only 2D and 3D images are supported')
+   return check
+      
 def numpy2firedrake(mesh, value, name=None):
    '''
    Convert np array (2d o 3d) into a function compatible with the mesh solver.
@@ -62,7 +112,9 @@ def numpy2firedrake(mesh, value, name=None):
    value: numpy array (2d or 3d) with images values
 
    returns: piecewise constant firedake function 
-   ''' 
+   '''
+   if (not compatible(mesh, value) ):
+      raise ValueError('Mesh and image are not compatible')
    
    # we flip vertically because images are read from left, right, top to bottom
    if mesh.ufl_cell().is_simplex():
@@ -82,27 +134,23 @@ def numpy2firedrake(mesh, value, name=None):
                d.array = triangles_image
             if name is not None:
                img_function.rename(name,name)
-      elif fd.mesh.geometric_dimension() == 3:
+      elif mesh.geometric_dimension() == 3:
+            value = np.flip(value,0)
             # copy the value to copy the voxel value
             # flatten the numpy matrix (EF: I don't know why swapaxes is needed)
-            print('reshaping')
             flat_value = value.swapaxes(0,2).flatten()
-            print('reshaping done')
-            ncopies = mesh.num_cells() // (value.shape[0]*value.shape[1]*value.shape[2])
+            ncopies = mesh.num_cells() // value.size
             copies = np.zeros([ncopies,len(flat_value)])
             for i in range(ncopies):
-               print(f'{i=}')
                copies[i,:] = flat_value[:]
-            print('reshaping')
             cells_values = copies.swapaxes(0,1).flatten()
-            print('reshaping done')
             # define function
-            DG0 = FunctionSpace(mesh,'DG',0)
-            img_function = Function(DG0, val=cells_values, name=name)
+            DG0 = fd.FunctionSpace(mesh,'DG',0)
+            img_function = fd.Function(DG0, val=cells_values, name=name)
 
    elif (mesh.ufl_cell().cellname() == 'quadrilateral'):
-      DG0 = FunctionSpace(mesh,'DG',0)
-      img_function = Function(DG0)
+      DG0 = fd.FunctionSpace(mesh,'DG',0)
+      img_function = fd.Function(DG0)
       value_flipped = np.flip(value,0)
       with img_function.dat.vec as d:
          d.array = value_flipped.flatten('F')
@@ -135,7 +183,6 @@ def image2numpy(img_name, normalize=True, invert=True, factor=1):
    img_grey = img_file.convert('L')
 
    # Save Greyscale values
-   print ('Pixels : '+str(img_grey.size[0])+' x '+ str(img_grey.size[1]))
    value = np.asarray(img_grey.getdata(), dtype=float)
    value = value.reshape((img_grey.size[1], img_grey.size[0]))
    if invert:
@@ -165,7 +212,7 @@ def numpy2image(numpy_matrix, image_path, normalized=True, inverted=True):
    img = Image.fromarray(np.uint8(copy),'L')
    img.save(image_path)
 
-def firedrake2numpy(function, dimensions):
+def firedrake2numpy(function):
    """
    Convert DG0 firedrake function to numpy array (2d or 3d).
    It works only for meshes genereted with RectangleMesh or BoxMesh.
@@ -174,20 +221,20 @@ def firedrake2numpy(function, dimensions):
    TODO: deduced dimensions from mesh. Probably from numbe of boundary facets.
    """
    mesh = function.function_space().mesh()
+   
    if mesh.ufl_cell().is_simplex():
       # Each pixel is splitted in two triangles.
       if mesh.geometric_dimension() == 2:
+         nx, ny = get_box_division(mesh)
          # get the values of the function
          with function.dat.vec_ro as f:
             value = f.array
-            print(f'{value.shape=}')
             # reshape the values in a matrix of size (2,nx*ny)
             value = value.reshape([-1,2])
             # average the values along the first dimension
             value = np.mean(value,1)
-            print(f'{value.shape=}')
-            # reshape the values in a matrix of size (nx,ny)
-            value = value.reshape([dimensions[0],dimensions[1]],order='F')
+            # reshape the values in a matrix of size ()
+            value = value.reshape([ny,nx],order='F')
 
             return np.flip(value,0)
             
@@ -200,39 +247,17 @@ def firedrake2numpy(function, dimensions):
       with function.dat.vec_ro as f:
          value = f.array
          # reshape the values in a matrix of size (nx,ny)
-         new_shape = [dimensions[0],dimensions[1]]
-         if mesh.geometric_dimension() == 3:
-            new_shape.append(self.nz)
-         value = value.reshape(new_shape,order='F')
+         new_shape = get_box_division(mesh)
+         nx = new_shape[0]
+         new_shape[0] = new_shape[1]
+         new_shape[1] = nx
+         value = value.reshape(new_shape, order='F')
          return np.flip(value,0)
 
 
-def matrix2function(value,cartesian_mesh):
-   """
-   Convert np matrix into a piecewise function 
-   defined ad the cartesina triangulation.
-   Each pixel is splitted in two triangles.
-   """ 
-   
-   # we flip vertically because images are read from left, right, top to bottom
-   value = np.flip(value,0)
-
-   # double the value to copy the pixel value to the triangles
-   double_value = np.zeros([2,value.shape[0],value.shape[1]])
-   double_value[0,:,:] = value[:,:]
-   double_value[1,:,:] = value[:,:]
-   triangles_image = double_value.swapaxes(0,2).flatten()
-   DG0 = fd.FunctionSpace(cartesian_mesh,'DG',0)
-   
-   img_function = fd.Function(DG0)
-   with img_function.dat.vec as d:
-      d.array = triangles_image
-
-   return img_function
-
 def function2image(function,image_path,colorbar=True,vmin=None,vmax=None):
    """
-   Print a fddrake function to grayscale image (0=white, >0=black)
+   Print a firedrake function to grayscale image (0=white, >0=black)
    using matplotlib tools in fddrake
    """
    fig, axes = plt.subplots()
@@ -242,7 +267,6 @@ def function2image(function,image_path,colorbar=True,vmin=None,vmax=None):
    if vmax is None:
       with function.dat.vec_ro as d:
          vmax = d.max()[1]
-   print(f'{vmin=}{vmax=}')
    
    colors = fd.tricontourf(function, 
       axes=axes, 
@@ -272,56 +296,4 @@ def function2image(function,image_path,colorbar=True,vmin=None,vmax=None):
       plt.gca().axis('tight')
    
    
-
-  
-   
    plt.savefig(image_path)#,bbox_inches='tight',transparent=True, pad_inches=0)
-   
-def corrupt_image(path_original,path_corrupted,path_masks):
-   """
-   Corrupt an image by applying masks
-   """
-   np_corrupted = i2d.image2matrix(Image.open(path_orig))
-   
-   for mask in path_masks:
-      np_corrupted -= i2d.image2matrix(Image.open(path_orig))
-   
-
-
-def image2grid(img_name,factor):
-   
-   img_file = Image.open(img_name)
-
-   if (factor <1):
-      # resize image
-      width, height = img_file.size
-      img_file = img_file.resize((int(width*factor),int(height*factor)), Image.ANTIALIAS)
-
-   # get original image parameters...
-   width, height = img_file.size
-   min_side = min(width,height)
-   mesh = fd.RectangleMesh(width,height,1,height/min_side,reorder=False)
-
-   return mesh
-   
-      
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-       source_path = sys.argv[1]
-       sink_path = sys.argv[2]
-
-       factor = 100
-       mesh = image2grid(sink_path,factor)
-       #source = image2function(source_path,mesh,factor)
-       sink = image2function(sink_path,mesh,factor)
-       #source.rename("source")
-       sink.rename("sink")
-       out_file = File("source_sink.pvd")
-       out_file.write(sink)
-
-
-       
-       
-       
-    else:
-        raise SystemExit("usage:  python  image2dat image data [grid]")
---
+++ tests/test_load_image.py
@@ -0,0 +1,36 @@
+import pytest
+import sys
+sys.path.append('../src/niot')
+import image2dat as i2d
+import numpy as np
+
+@pytest.mark.parametrize('img_path', ['7.png'])
+@pytest.mark.parametrize('mesh_type', ['simplicial','cartesian'])
+@pytest.mark.parametrize('normalizeRGB', [True, False])
+@pytest.mark.parametrize('inverteBW', [True, False])
+def test_convert_write(img_path, mesh_type, normalizeRGB, inverteBW):
+    """
+    Load an image and create a mesh
+    image -> numpy -> firedrake function-> numpy -> image 
+    Compare the results and assert equality.
+    """
+    # convert to matrix
+    np_img = i2d.image2numpy(img_path,normalize=normalizeRGB,invert=inverteBW)
+    
+    # create mesh
+    mesh = i2d.build_mesh_from_numpy(np_img, mesh_type=mesh_type)
+
+    # convert to firedrake
+    fire_img = i2d.numpy2firedrake(mesh, np_img)
+
+    # convert back to numpy
+    np_img_converted = i2d.firedrake2numpy(fire_img)
+
+    # write to file
+    i2d.numpy2image(np_img_converted,'test_load_image.png', normalized=normalizeRGB, inverted=inverteBW)
+    
+    # read stored image
+    np_img2 = i2d.image2numpy('test_load_image.png', normalize=normalizeRGB, invert=inverteBW)
+    
+    # compare saved and stored images
+    assert np.allclose(np_img,np_img2)    
