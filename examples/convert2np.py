78d52a83f357afe6a662b2e9ea4dbd9256bc4176
enrico facca <enrico.facca@gmail.com>
2024-01-10 17:48:16 +0100

    script to get thickness from fiji

---
+++ examples/convert2np.py
@@ -0,0 +1,50 @@
+import numpy as np
+import sys
+from niot import image2dat as i2d
+from niot.utilities import save2pvd
+
+# read file path
+file_path = sys.argv[1]
+file_skeleton = sys.argv[2]
+
+
+
+thickness = np.loadtxt(file_path, dtype=np.float32, delimiter='\t', skiprows=0)
+# replace Nan with 0 in data
+thickness = np.nan_to_num(thickness)
+# save as npy file
+np.save('thickness.npy', thickness)
+np_skeleton = i2d.image2numpy(file_skeleton,normalize=True,invert=True)
+
+print(np_skeleton.shape)
+pixel_h = 1.0 / thickness.shape[1]
+
+thickness_skeleton = thickness * np_skeleton * pixel_h
+np.save('thickness_skeleton.npy', thickness_skeleton)
+i2d.numpy2image(thickness_skeleton,'thickness_skeleton.png', normalized=False, inverted=True)
+
+
+thickness_skeleton_read = i2d.image2numpy('thickness_skeleton.png',normalize=False,invert=True)
+# convert to firedrake function and save
+mesh = i2d.build_mesh_from_numpy(thickness, mesh_type='cartesian')
+
+
+# save inputs    
+thickness_fire = i2d.numpy2firedrake(mesh, thickness, name="thickness_fire")
+np_skeleton_fire = i2d.numpy2firedrake(mesh, np_skeleton, name="skeleton_fire")
+thickness_skeleton_fire = i2d.numpy2firedrake(mesh, thickness_skeleton, name="thickness_skeleton_fire")
+thickness_skeleton_read_fire = i2d.numpy2firedrake(mesh, thickness_skeleton_read, name="thickness_skeleton_read_fire")
+
+# save inputs
+save2pvd([
+    thickness_fire,
+    np_skeleton_fire,
+    thickness_skeleton_fire,
+    thickness_skeleton_read_fire,
+    ], 
+    'thickness.pvd'
+    )
+
+
+
+exit()
