7faddd469e45088762780e9a875e4d14f91df4d8
enrico facca <enrico.facca@gmail.com>
2024-01-24 13:11:20 +0100

    not savinf vtu

--- examples/change_resolution.py
+++ examples/change_resolution.py
@@ -11,13 +11,13 @@ out_np = i2d.image2numpy(file_in,normalize=True,invert=True,factor=2**nref)
 i2d.numpy2image(out_np,file_out, normalized=True, inverted=True)    
 
 # convert to firedrake function and save
-mesh_old = i2d.build_mesh_from_numpy(in_np, mesh_type='cartesian')
-fire_old = i2d.numpy2firedrake(mesh_old, in_np, name="new")
+#mesh_old = i2d.build_mesh_from_numpy(in_np, mesh_type='cartesian')
+#fire_old = i2d.numpy2firedrake(mesh_old, in_np, name="new")
 
 
-mesh_new = i2d.build_mesh_from_numpy(out_np, mesh_type='cartesian')
-fire_new = i2d.numpy2firedrake(mesh_new, out_np, name="new")
+#mesh_new = i2d.build_mesh_from_numpy(out_np, mesh_type='cartesian')
+#fire_new = i2d.numpy2firedrake(mesh_new, out_np, name="new")
 
 # save inputs
-save2pvd([fire_old,], 'old.pvd')
-save2pvd([fire_new,], 'new.pvd')
+#save2pvd([fire_old,], 'old.pvd')
+#save2pvd([fire_new,], 'new.pvd')
