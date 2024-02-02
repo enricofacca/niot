683602ed75ea47afbb33da2bc70300991179aace
enricofacca <enrico.facca@uib.no>
2024-02-02 17:08:19 +0100

    test for long run

--- examples/corrupt_and_reconstruct.py
+++ examples/corrupt_and_reconstruct.py
@@ -372,7 +372,7 @@ def corrupt_and_reconstruct(np_source,
     # optimization
     niot_solver.ctrl_set('optimization_tol', 5e-3)
     niot_solver.ctrl_set('constraint_tol', 1e-5)
-    niot_solver.ctrl_set('max_iter', 5000)
+    niot_solver.ctrl_set('max_iter', 1)
     niot_solver.ctrl_set('max_restart', 4)
     niot_solver.ctrl_set('verbose', 2)  
     
