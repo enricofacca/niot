3e0f273a83c6738fcc99dc89ce375c93956a8d28
enricofacca <enrico.facca@uib.no>
2024-02-29 12:00:17 +0100

    current jobs

--- examples/run_parallel.py
+++ examples/run_parallel.py
@@ -30,20 +30,20 @@ mask=['mask_medium.png']#,'mask_small.png']
 nref=[0]
 fems = ['DG0DG0']
 gamma = [0.5]#, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-wd = [1.5e-1]#,1e-1,1e0,1e1]#, 5e5, 1e3, 5e3, 1e4]#0,1e0,1e4]#,1e-5,1e-3,1e-1
+wd = [1e-4,1e-3,1e-2,1.2e-2,5e-2,1e-1]#,1e-1,1e0,1e1]#, 5e5, 1e3, 5e3, 1e4]#0,1e0,1e4]#,1e-5,1e-3,1e-1
 wr = [0]
-ini = [0]
+ini = [0,1e5]
 network_file = ['network.png']
 #network_file = ['network_artifacts.png']
 #network_file = ['mup3.0e+00zero1.0e+01.npy']
 #,'mupou3.0e+00zero1.0e+01.npy']#,'mucnstp3.0e+00zero5.0e+05.npy','muthickness.npy']
 #network_file = ['mup3.0e+00zero1.0e+07.npy']#,'mupou3.0e+00zero1.0e+07']#,'mucnstp3.0e+00zero1.0e+01.npy']#,'network.png']
 #network_file = ['network.png']#,'thickness.npy']
-conf = ['MASK','ONE']#,'MASK','CORRUPTED']
+conf = ['ONE','MASK']#,'CORRUPTED']
 maps = [
-#    {'type':'identity','scaling': 10}, 
+    {'type':'identity','scaling': 10}, 
     {'type':'pm', 'sigma': 1e-3, 'exponent_m': 2.0, 'scaling': 10},
-    {'type':'pm', 'sigma': 1e-4, 'exponent_m': 2.0, 'scaling': 10},
+#    {'type':'pm', 'sigma': 1e-4, 'exponent_m': 2.0, 'scaling': 10},
 ]
 
     #    {'type':'heat', 'sigma': 1e-4, 'scaling': 100},
