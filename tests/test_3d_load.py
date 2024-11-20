b89b2cbb35cbaa2a72da810c765f7b3ada96f2ea
enrico facca <enrico.facca@gmail.com>
2024-11-20 15:21:42 +0100

    test for converting data in 3d to firedrake

--- tests/test_3d_load.py
+++ tests/test_3d_load.py
@@ -12,6 +12,7 @@ from niot import image2dat as i2d
 import numpy as np
 from mpi4py.MPI import MAX,MIN
 
+import pytest
 
 save_pvd = True
 save_vtr = False
@@ -29,13 +30,16 @@ example[0,:,0] = 2.0#y_array[:]
 example[0,0,:] = 3.0#z_array[:]
 example[0,0,0] = 6.0
 
-def explicit_expression(x,y,z):
-    return (3 * conditional(x<1.0/nx,1,0)*conditional(y<1.0/ny,1,0)
-            + 2 * conditional(x<1.0/nx,1,0)*conditional(z<1.0/nz,1,0)
-            + 1 * conditional(y<1.0/ny,1,0)*conditional(z<1.0/nz,1,0) )
-
 
 lengths = [1,1,1]
+def explicit_expression(x,y,z, lengths):
+    Lx,Ly,Lz = lengths
+    return (3 * conditional(x<Lx/nx,1,0)*conditional(y<Ly/ny,1,0)
+            + 2 * conditional(x<Lx/nx,1,0)*conditional(z<Lz/nz,1,0)
+            + 1 * conditional(y<Ly/ny,1,0)*conditional(z<Lz/nz,1,0) )
+
+
+
 
 
 
@@ -43,7 +47,7 @@ lengths = [1,1,1]
 i2d.numpy2vtr(example, lengths, "test3d_np", name='source')
 
 # define cartesian mesh
-def test_3d_numpy_simplicial_mesh(mesh, example):
+def check_3d_numpy_simplicial_mesh(mesh, example):
    # get number of cells
     ncells = mesh.num_cells()
     # get number of vertices
@@ -53,15 +57,16 @@ def test_3d_numpy_simplicial_mesh(mesh, example):
         assert(ncells == 6 * np.prod(example.shape)) # 6 tetrahedra per cartesian cell
         assert(nvertices == np.prod([n+1 for n in example.shape]))
 
-def test_3d_numpy_cartesian_mesh(mesh, example):
+def check_3d_numpy_cartesian_mesh(mesh, example):
     # get number of cells
-    if mesh.extruded:
+    if hasattr(mesh,"extruded"):
         ncells = mesh.num_cells() * (mesh.layers-1)
         nvertices = mesh.num_vertices() * (mesh.layers)
     else:
         ncells = mesh.num_cells() 
         nvertices = mesh.num_vertices()
 
+    
     # get number of vertices
     if mesh.comm.size == 1:
         assert(ncells == np.prod(example.shape))
@@ -69,7 +74,7 @@ def test_3d_numpy_cartesian_mesh(mesh, example):
 
 
 
-def test_coordinates(mesh, lengths):   
+def check_coordinates(mesh, lengths):   
     np_coordinates = mesh.coordinates.dat.data_ro
     x_max_p = np.max(np_coordinates[:,0])
     x_min_p = np.min(np_coordinates[:,0])
@@ -96,44 +101,104 @@ def test_coordinates(mesh, lengths):
     assert( np.isclose(z_min,0.0))
     assert( np.isclose(z_max,lengths[2]))
 
-    
+mesh_types = ['simplicial','cartesian','cartesian_extruded']
+arrays = [example]
+lenghts_box = [lengths,[1.0,2.0,3.0]]
+
+@pytest.mark.parametrize("mesh_type", mesh_types)
+@pytest.mark.parametrize("example", arrays)
+@pytest.mark.parametrize("lengths", lenghts_box)
+def test_3d_mesh(mesh_type, example, lengths):
+    print(f"Testing {mesh_type}")
+    if mesh_type == 'simplicial':
+        mesh = i2d.build_mesh_from_numpy(example, mesh_type='simplicial',lengths=lengths)  
+        check_3d_numpy_simplicial_mesh(mesh, example)
+        check_coordinates(mesh, lengths)
+    elif "cartesian" in mesh_type :
+        extrude = ("extruded" in mesh_type)
+        print(f"extrude {extrude}")
+        mesh = i2d.build_mesh_from_numpy(example, mesh_type='cartesian',lengths=lengths, extrude=extrude)
+        print(f"mesh {mesh}")
+        
+def h_length(V,mode):
+    mesh = V.mesh()
+    if (mode == 'cell_distance'):
+        x,y = mesh.coordinates
+        x_func = interpolate(x, V)
+        y_func = interpolate(y, V)
+        h_len = sqrt(jump(x_func)**2 + jump(y_func)**2)
+    elif mode == 'cell_diameter':
+        # implemation as
+        alpha = 1.0
+        h = CellDiameter(mesh)/sqrt(2)
+        h_len = avg(h)/alpha
+    else:
+        raise ValueError('mode must be - center_distance, cell_diameter')
+    return h_len
+
+        
+        check_3d_numpy_cartesian_mesh(mesh, example)
+        check_coordinates(mesh, lengths)
+
+    # convert to firedrake
+    source_fire = i2d.numpy2firedrake(mesh, example, 'source',lengths=lengths)
+    cartesian_mesh = i2d.build_mesh_from_numpy(example, mesh_type='cartesian',lengths=lengths)
+    test_source = Function(FunctionSpace(cartesian_mesh, "DG", 0))
+    source_fire_dg0 = Function(FunctionSpace(cartesian_mesh, "DG", 0))
+    source_fire_dg0.interpolate(source_fire)
+
+    # test against explicit expression
+    x,y,z = SpatialCoordinate(cartesian_mesh)
+    test_source.interpolate( explicit_expression(x,y,z,lengths) )   
+    assert (np.isclose( assemble((source_fire_dg0 - test_source)**2*dx), 0.0))
+
+    # convert to numpy
+    source_numpy = i2d.firedrake2numpy(source_fire_dg0)
+
+    assert(np.allclose(example,source_numpy))
 
 
+if __name__ == '__main__':
+    test_3d_mesh(mesh_types[0], example, lengths)
+    test_3d_mesh(mesh_types[1], example, lengths)
+    test_3d_mesh(mesh_types[2], example, lengths)
 
-# create
-mesh = i2d.build_mesh_from_numpy(example, mesh_type='simplicial',lengths=lengths)  
-test_3d_numpy_simplicial_mesh(mesh, example)
-test_coordinates(mesh, lengths)
+    # create
+    mesh = i2d.build_mesh_from_numpy(example, mesh_type='simplicial',lengths=lengths)  
+    check_3d_numpy_simplicial_mesh(mesh, example)
+    check_coordinates(mesh, lengths)
 
-# create cartesian mesh
-cartesian_mesh = i2d.build_mesh_from_numpy(example, mesh_type='cartesian',lengths=lengths)
-test_3d_numpy_cartesian_mesh(cartesian_mesh, example)
-test_coordinates(cartesian_mesh, lengths)
+    # create cartesian mesh
+    cartesian_mesh = i2d.build_mesh_from_numpy(example, mesh_type='cartesian',lengths=lengths, extrude=False)
+    print(f"{cartesian_mesh.ufl_cell().is_simplex()=}")
 
-
-# convert to firedrake
-source_fire = i2d.numpy2firedrake(mesh, example, 'source',lengths=lengths)
-test_source = Function(FunctionSpace(cartesian_mesh, "DG", 0))
-source_fire_dg0 = Function(FunctionSpace(cartesian_mesh, "DG", 0))
-source_fire_dg0.interpolate(source_fire)
-
-
-# test against explicit expression
-x,y,z = SpatialCoordinate(cartesian_mesh)
-test_source.interpolate( explicit_expression(x,y,z) )   
-assert (np.isclose( assemble((source_fire_dg0 - test_source)**2*dx), 0.0))
+    check_3d_numpy_cartesian_mesh(cartesian_mesh, example)
+    check_coordinates(cartesian_mesh, lengths)
 
 
-# convert to numpy
-source_numpy = i2d.firedrake2numpy(source_fire_dg0)
-
-assert(np.allclose(example,source_numpy))
-
-if save_pvd:
-    out_file = VTKFile('test3d_firedrake.pvd')
-    out_file.write(source_fire)
+    # convert to firedrake
+    source_fire = i2d.numpy2firedrake(mesh, example, 'source',lengths=lengths)
+    test_source = Function(FunctionSpace(cartesian_mesh, "DG", 0))
+    source_fire_dg0 = Function(FunctionSpace(cartesian_mesh, "DG", 0))
+    source_fire_dg0.interpolate(source_fire)
 
 
-if save_vtr:
-    i2d.numpy2vtr(source_numpy, [1,1,1], "test3d_npfiredrakenp", name='image')
+    # test against explicit expression
+    x,y,z = SpatialCoordinate(cartesian_mesh)
+    test_source.interpolate( explicit_expression(x,y,z) )   
+    assert (np.isclose( assemble((source_fire_dg0 - test_source)**2*dx), 0.0))
+
+
+    # convert to numpy
+    source_numpy = i2d.firedrake2numpy(source_fire_dg0)
+
+    assert(np.allclose(example,source_numpy))
+
+    if save_pvd:
+        out_file = VTKFile('test3d_firedrake.pvd')
+        out_file.write(source_fire)
+
+
+    if save_vtr:
+        i2d.numpy2vtr(source_numpy, [1,1,1], "test3d_npfiredrakenp", name='image')
 
