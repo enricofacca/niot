257b6be09f2ea60599d19740cdd25e1cb03c6985
enrico facca <enrico.facca@gmail.com>
2024-11-22 11:27:23 +0100

    more aggressive time stepping

--- src/niot/niot.py
+++ src/niot/niot.py
@@ -364,8 +364,8 @@ def set_step(increment,
         else:
             step = max(min(1.0 / d_max, upper_bound), lower_bound)
     elif (type == 'adaptive2'):
-        order_down = -0.5
-        order_up = 0.5
+        order_down = -0.8
+        order_up = 0.8
         r = increment / state
         r_np = r.array
         if np.min(r_np) < 0:
