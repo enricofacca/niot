ac779e94502a81714819c277f0fceed45d0a8909
enrico facca <enrico.facca@gmail.com>
2024-11-29 14:41:55 +0100

    script to converto nii.gz file into vtr

--- examples/2024mri/data/nii2vtr.py
+++ examples/2024mri/data/nii2vtr.py
@@ -3,17 +3,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pyevtk.hl import gridToVTK, writeParallelVTKGrid
 from niot import image2dat as i2d
+import sys
 
 #epi_img = nib.load('T1.nii.gz')
 #epi_img_data = epi_img.get_fdata()
 #print(epi_img_data.shape)
 #np.save('T1.npy',epi_img_data)
-epi_img = nib.load('../../../tests/mri/TOF.nii.gz')
+
+nii_file = sys.argv[1]
+try:
+    data_name = sys.argv[2]
+except:
+    data_name = 'data'
+vtr_file = nii_file.replace('.nii.gz', '')
+
+epi_img = nib.load(nii_file)
 epi_img_data = epi_img.get_fdata()
-#np.save('TOF.npy',epi_img_data)
 hdr = epi_img.header
 print(hdr)
-
+print(epi_img.affine)
 
 
 nx, ny, nz = epi_img_data.shape
@@ -21,9 +29,8 @@ dx, dy, dz = epi_img.header['pixdim'][1:4]
 lx, ly, lz = nx * dx, ny * dy, nz * dz
 
 # Coordinates
-i2d.numpy2vtr(epi_img_data, [lx, ly, lz], "TOF", name='tof')
+i2d.numpy2vtr([epi_img_data], [lx, ly, lz], vtr_file, names=[data_name])
 exit()
-
 
 #epi_img = nib.load('QSM.nii.gz')
 #epi_img_data = epi_img.get_fdata()
