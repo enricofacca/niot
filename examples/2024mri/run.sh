3bf68f8e4dd772afac0533ecdb8227a1000d76af
enrico facca <enrico.facca@gmail.com>
2025-03-07 10:43:42 +0100

    included runs.h

---
+++ examples/2024mri/run.sh
@@ -0,0 +1,38 @@
+#
+# bash script to run the 2024mri example 
+#
+# Usage: ./run.sh c dir
+option=$1
+c=$2
+np=$3
+ne=$4
+
+label=$(date "+%Y%m%d_%H%M%S")
+
+# copy the json file to the niot directory
+cp $1 ./niot/examples/2024mri/options.json
+
+# source the environment
+source venv-firedrake/bin/activate
+
+# move into the niot directory
+cd niot
+
+# udapte the niot package
+git checkout 3dmri
+git pull
+
+# check if the niot package is installed
+cd examples/2024mri/
+
+# checkif h5 with mesh tof t1 qsm, with the proper number of processors exists
+#if [ ! -f ${dir}/mri_${np}.h5 ]; then
+#    # run the script to generate the mesh 
+#    mpiexec -n ${ne} python ./input2h5.py ${c} ${dir}
+#fi
+
+# run the recostruction
+formatted_c=$(printf "%02d" ${c})
+dir="./data/subj0_c${formatted_c}/"
+nohup mpiexec -n ${np} python ./new_reconstruct_tof.py --mri ${dir} --option options.json --n_ensemble ${ne} > submitted/${label}_c${c}.out 2> submitted/${label}_c${c}.err &
+ 
