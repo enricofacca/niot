075a2f1ec80eec4ff403b4174e1fa0c397d999b8
enrico facca <enrico.facca@gmail.com>
2025-03-19 15:27:53 +0100

    install firedrake and niot

---
+++ examples/2024mri/install_firedrake_and_niot.sh
@@ -0,0 +1,33 @@
+#
+# 
+#
+sudo apt -y update
+sudo apt -y upgrade
+
+
+# PETSC
+curl -O https://raw.githubusercontent.com/firedrakeproject/firedrake/master/scripts/firedrake-configure
+sudo apt -y install $(python3 firedrake-configure --show-system-packages)
+sudo apt -y install python3.12-venv
+export PETSC_CONFIGURE_OPTIONS="--download-hypre --download-spai"
+git clone --depth 1 https://github.com/firedrakeproject/petsc.git
+cd petsc/
+python3 ../firedrake-configure --show-petsc-configure-options| xargs -L1 ./configure
+make PETSC_DIR=/home/ubuntu/petsc PETSC_ARCH=arch-firedrake-default all
+make PETSC_DIR=/home/ubuntu/petsc PETSC_ARCH=arch-firedrake-default check
+cd ..
+
+# FIREDRAKE
+python3 -m venv venv-firedrake
+. venv-firedrake/bin/activate
+export $(python3 firedrake-configure --show-env)
+pip install --no-binary h5py "firedrake @ git+https://github.com/firedrakeproject/firedrake.git#[test]"
+firedrake-check 
+
+# NIOT
+git clone https://github.com/enricofacca/niot.git
+cd niot/
+git checkout 3dmri
+pip install -e .
+cd tests/
+pytest test_DirichletBC.py 
