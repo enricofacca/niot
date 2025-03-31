45cb5d1ff00f8b204ca9c6e912bdc31a550f7158
enrico facca <enrico.facca@gmail.com>
2025-03-31 18:01:00 +0200

    removed print

--- examples/2024mri/nii2vtr.py
+++ examples/2024mri/nii2vtr.py
@@ -37,7 +37,6 @@ for i, nii_file in enumerate(nii_files):
     dx, dy, dz = epi_img.header['pixdim'][1:4]
     offset = epi_img.affine[:3, 3]
 
-    print(i ,nii_file, epi_img_data.shape)
     if i == 0:
         old_shape = epi_img_data.shape
         old_size = epi_img.header['pixdim'][1:4]
