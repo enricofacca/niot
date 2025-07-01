35390f197db6ae231e04f8573788e507b54b9273
enrico facca <enrico.facca@gmail.com>
2025-07-01 12:57:24 +0200

    checking height

--- examples/2024mri/PorousMediaMap.py
+++ examples/2024mri/PorousMediaMap.py
@@ -123,7 +123,7 @@ def skeletonthickness_to_tubular(skeleton_thickness, h, cond_zero, exponent_p, v
         PETSc.Sys.Print(f'img_height={height_min:.1e} height_max={height_max:.1e} ')
     
 
-    PETSc.Sys.Print(f'{cond.dat.data_ro.min():.2e}<=IMG<={cond.dat.data_ro.max():.2e}')
+    PETSc.Sys.Print(f'{cond.dat.data_ro.min():.2e}<=cond<={cond.dat.data_ro.max():.2e}')
 
     pm_map = conductivity2image.PorousMediaMap(
         space,
@@ -135,7 +135,7 @@ def skeletonthickness_to_tubular(skeleton_thickness, h, cond_zero, exponent_p, v
     image = pm_map(cond)
     name = f'img_pm'
     image.rename(name)
-    PETSc.Sys.Print(f'{image.dat.data_ro.min():.2e}<=IMG<={image.dat.data_ro.max():.2e}')
+    PETSc.Sys.Print(f'{image.dat.data_ro.min():.2e}<=img<={image.dat.data_ro.max():.2e}')
     
     if save:
         filename = f'images_img0.pvd'
@@ -196,7 +196,7 @@ if (__name__ == '__main__'):
             cond_zero=args.cond,
             verbose=True,
             save=False)
-    print(f'{tubular.dat.data_ro.min():.2e}<=IMG<={tubular.dat.data_ro.max():.2e}')
+    print(f'{tubular.dat.data_ro.min():.3e}<=IMG<={tubular.dat.data_ro.max():.3e}')
     PETSc.Sys.Print(f'{tubular.dat.data_ro.min():.2e}<=IMG<={tubular.dat.data_ro.max():.2e}')
     
     
