81b40f311277da9a880ff1f8449e5e47e3a9f74b
enrico facca <enrico.facca@gmail.com>
2025-07-22 18:18:57 +0200

    bug

--- examples/2024mri/new_reconstruct_tof.py
+++ examples/2024mri/new_reconstruct_tof.py
@@ -1058,7 +1058,7 @@ def experiment(args):
             if combination["map"]["type"] != "identity":
                 filename = f"{label_dir}/image_reconstruction_{file_label}.nii.gz"
                 save_as_nifti(niot_solver.reconstruction, affine, filename)
-                for i, img in enumerate(self.tdens2image_map.intermediate_images):
+                for i, img in enumerate(niot_solver.tdens2image_map.intermediate_images):
                     filename = f"{label_dir}/image_intermediate_{file_label}_{i}.nii.gz"
                     save_as_nifti(img, affine, filename)
                 
