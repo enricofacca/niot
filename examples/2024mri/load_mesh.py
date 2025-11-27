0fdb8dcb6dfc21d16498853691a804f40cf97273
enrico facca <enrico.facca@gmail.com>
2025-11-27 18:59:30 +0100

    test with timing

--- examples/2024mri/load_mesh.py
+++ examples/2024mri/load_mesh.py
@@ -1,15 +1,23 @@
 from firedrake import *
-
+from firedrake.petsc import PETSc
+from time import time
 #
 # load data
 #
 comm = COMM_WORLD
 n_proc = COMM_WORLD.size
 h5_file = f"inputs_nproc{n_proc}.h5"
+
 with CheckpointFile(h5_file, 'r',comm=comm) as afile:
-    mesh = afile.load_mesh("RelabeledMesh")
+    start = time()
+    mesh = afile.load_mesh("relabeled_mesh")
+    PETSc.Sys.Print(f" Loaded mesh in {time()-start:.2f} seconds")
+    start = time()
     tof = afile.load_function(mesh, "tof_mesh")
+    PETSc.Sys.Print(f" Loaded tof in {time()-start:.2f} seconds")
+    start = time()
     t1 = afile.load_function(mesh, "t1_mesh")
+    PETSc.Sys.Print(f" Loaded t1 in {time()-start:.2f} seconds")
     main_network = afile.load_function(mesh, "main_network_mesh")
 
 V = FunctionSpace(mesh, "CG", 1)
