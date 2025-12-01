3989ced2d8c60ff3220d71b5371e479833ef2422
enrico facca <enrico.facca@gmail.com>
2025-12-01 15:14:54 +0100

    smaller faces

--- examples/2024mri/build_mesh.py
+++ examples/2024mri/build_mesh.py
@@ -223,11 +223,12 @@ def setup(mri_directory,
         
         
         PETSc.Sys.Print("volex size:", hx, hy, hz)
-        scale = 2
+        scale = 1
         mesh_pygal = pygalmesh.generate_from_array(
                 mask,
                 voxel_size, 
                 max_facet_distance=scale*hx,
+                max_circumradius_edge_ratio=3.0,
                 max_cell_circumradius={
                     "default": scale*16*hx, 
                     label_main: scale*hx,
