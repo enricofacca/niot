060918787862a51f7293057669b95d66e10c4378
enrico facca <enrico.facca@gmail.com>
2025-12-01 18:11:45 +0100

    sppliiting

--- examples/2024mri/mesh2h5.py
+++ examples/2024mri/mesh2h5.py
@@ -115,6 +115,10 @@ def setup(mri_directory,
     mesh.coordinates.dat.data[:, 1] -= offset[1]
     mesh.coordinates.dat.data[:, 2] -= offset[2]
     PETSc.Sys.Print("Offset completed")
+    PETSc.Sys.Print(f" xmin {mesh.coordinates.dat.data[:,0].min():.2f}, xmax {mesh.coordinates.dat.data[:,0].max():.2f}")
+    PETSc.Sys.Print(f" ymin {mesh.coordinates.dat.data[:,1].min():.2f}, ymax {mesh.coordinates.dat.data[:,1].max():.2f}")
+    PETSc.Sys.Print(f" zmin {mesh.coordinates.dat.data[:,2].min():.2f}, zmax {mesh.coordinates.dat.data[:,2].max():.2f}")
+    PETSc.Sys.Print(f" lengths: {lengths}") 
     
     PETSc.Sys.Print("Numpy to Firedrake Functions on Cartesian grid", end="")
     cartesian_mesh =  i2d.cartesian_grid_3d(dimensions,lengths)
