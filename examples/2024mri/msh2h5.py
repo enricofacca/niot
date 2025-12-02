059ed6c95a1ca0bb04f109062e623057f75fc047
enrico facca <enrico.facca@gmail.com>
2025-12-02 18:20:27 +0100

    test h5 createing

--- examples/2024mri/msh2h5.py
+++ examples/2024mri/msh2h5.py
@@ -8,6 +8,7 @@ def convert_msh_to_h5(input_directory, out_directory):
     start = time.time() 
     PETSc.Sys.Print(f"Mesh loading started...", end="")
     mesh = Mesh(os.path.join(input_directory,"brain_main.msh"))
+    mesh.name = "mesh"
     PETSc.Sys.Print(f" completed in {time.time()-start:.2e} s")
     print("New boundary markers (should include 99):", mesh.topology.exterior_facets.unique_markers)
     nproc = PETSc.COMM_WORLD.getSize()
