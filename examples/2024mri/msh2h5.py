6ff90fc52dd9c3ad7be9d83058bf0ceb2da6446b
enrico facca <enrico.facca@gmail.com>
2025-12-02 17:44:12 +0100

    convert msh to h5 checkpointfile

---
+++ examples/2024mri/msh2h5.py
@@ -0,0 +1,30 @@
+from firedrake import Mesh, CheckpointFile, COMM_WORLD
+from firedrake.petsc import PETSc
+import time
+import os
+import argparse
+
+def convert_msh_to_h5(input_directory, out_directory):
+    start = time.time() 
+    mesh = Mesh(os.path.join(input_directory,"brain_main.msh"))
+    PETSc.Sys.Print(f"Mesh loaded in {time.time()-start:.2e} s")
+
+    nproc = PETSc.COMM_WORLD.getSize()
+
+    start = time.time() 
+    h5_filename = os.path.join(out_directory, f"brain_main_{nproc:04d}.h5")
+    with CheckpointFile(h5_filename, 'w', comm=COMM_WORLD) as afile:
+            afile.save_mesh(mesh,"relabeled_mesh")
+            PETSc.Sys.Print(f" mesh saved to {h5_filename} in {time.time()-start:.2e} s")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert .msh mesh to .h5 format.")
+    parser.add_argument("-i","--input_directory", type=str, help="Directory containing the input .msh file.")
+    parser.add_argument("-o","--out_directory", type=str, help="Directory to save the output .h5 file.")
+    args = parser.parse_args()
+
+    input_directory = args.input_directory
+    out_directory = args.out_directory
+
+    convert_msh_to_h5(input_directory, out_directory)
