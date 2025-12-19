394696a8e13a5fa4acee9210490493085188c788
enrico facca <enrico.facca@gmail.com>
2025-12-19 11:24:56 +0100

    debugging callback

--- src/niot/conductivity2image.py
+++ src/niot/conductivity2image.py
@@ -448,11 +448,12 @@ class PorousMediaMap(Conductivity2ImageMap):
         
         def img_bounds(X,F):
             PETSc.Sys.Print("Porous Media Image bounds: ")
-            with X.dat.vec_ro as v:
-                min_v = v.min()[1]
-                max_v = v.max()[1]
-                msg = "".join([f'{min_v:2.1e}','<=PM IMG <=',f'{max_v:2.1e}'])
-                PETSc.Sys.Print(msg)
+            min_v = X.min()[1]
+            max_v = X.max()[1]
+            msg = "".join([f'{min_v:2.1e}','<=PM IMG <=',f'{max_v:2.1e}'])
+            PETSc.Sys.Print(msg)
+            print(X.comm.rank,msg)
+
                 
 
         
