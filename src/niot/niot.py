5eeb81ac7c7da968e70a1182cd9ec0c69f1aca6d
enrico facca <enrico.facca@gmail.com>
2025-12-29 11:55:20 +0100

    more aggressive

--- src/niot/niot.py
+++ src/niot/niot.py
@@ -711,9 +711,9 @@ class NiotSolver:
                             # tuning parameters for the multigrid
                             # https://mooseframework.inl.gov/releases/moose/2021-09-15/application_development/hypre.html
                             "pc_hypre_type": "boomeramg",
-                            "pc_hypre_boomeramg_strong_threshold": 0.6,#0.75,
+                            "pc_hypre_boomeramg_strong_threshold": 0.75,
                             "pc_hypre_boomeramg_max_iter": 1,
-                            "pc_hypre_boomeramg_agg_nl": 0,
+                            "pc_hypre_boomeramg_agg_nl": 3,
                             "pc_hypre_boomeramg_interp_type": "ext+i",  # "classic" or "ext+i"
                         }
             petsc_controls.update(hypre_ctrl_3d)
